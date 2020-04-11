c156207dee4f1e9598a38830c8aba2fa8d4308ed
hotio <26902309+hotio@users.noreply.github.com>
2020-04-11 14:34:40 +0200

    tweak build

--- linux-arm64.Dockerfile
+++ linux-arm64.Dockerfile
@@ -2,7 +2,7 @@ FROM hotio/base@sha256:f1629f6864be54d0d4ed469bbc5fc20f8f7a92121fbc536368c9279e2
 
 ARG DEBIAN_FRONTEND="noninteractive"
 
-ARG UNPACKERR_VERSION=0.7.0
+ARG UNPACKERR_VERSION
 
 # install app
 RUN gzfile="/tmp/unpackerr.gz" && curl -fsSL -o "${gzfile}" "https://github.com/davidnewhall/unpackerr/releases/download/v${UNPACKERR_VERSION}/unpackerr.arm64.linux.gz" && gunzip -c "${gzfile}" | dd of="${APP_DIR}/unpackerr" && chmod 755 "${APP_DIR}/unpackerr" && rm "${gzfile}"
--- update.sh
+++ update.sh
@@ -12,7 +12,6 @@ if [[ ${1} == "checkdigests" ]]; then
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/davidnewhall/unpackerr/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG UNPACKERR_VERSION=.*$/ARG UNPACKERR_VERSION=${version}/g" {} \;
-    sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
+    sed -i "s/{UNPACKERR_VERSION=[^}]*}/{UNPACKERR_VERSION=${version}}/g" .drone.yml
     echo "##[set-output name=version;]${version}"
 fi
