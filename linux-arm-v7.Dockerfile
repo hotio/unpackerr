b18fc9e14da23bdfd6ca353f31ea0f0e3106cc87
hotio <26902309+hotio@users.noreply.github.com>
2020-09-13 20:56:40 +0200

    rework build

--- linux-arm-v7.Dockerfile
+++ linux-arm-v7.Dockerfile
@@ -1,28 +1,13 @@
 FROM golang:stretch as builder
 
-ARG UNPACKERR_VERSION
+ARG VERSION
 
 RUN git clone -n https://github.com/davidnewhall/unpackerr.git /unpackerr && cd /unpackerr && \
-    git checkout v${UNPACKERR_VERSION} -b hotio && \
-    COMMIT_DATE=$(date -u --date=@$(git show -s --format=%ct v${UNPACKERR_VERSION}) +'%Y-%m-%dT%H:%M:%SZ') && sed -i "s/DATE=.*/DATE=${COMMIT_DATE}/g" .metadata.sh && \
+    git checkout v${VERSION} -b hotio && \
+    COMMIT_DATE=$(date -u --date=@$(git show -s --format=%ct v${VERSION}) +'%Y-%m-%dT%H:%M:%SZ') && sed -i "s/DATE=.*/DATE=${COMMIT_DATE}/g" .metadata.sh && \
     CGO_ENABLED=0 make unpackerr.armhf.linux
 
 FROM hotio/base@sha256:60aaa76ce60cbb19050c556a68f6d20b917442d4b1c97dc5c79f8e06c7c96c82
 COPY --from=builder /unpackerr/unpackerr.armhf.linux ${APP_DIR}/unpackerr
 RUN chmod 755 "${APP_DIR}/unpackerr"
 COPY root/ /
-
-ARG LABEL_CREATED
-LABEL org.opencontainers.image.created=$LABEL_CREATED
-ARG LABEL_TITLE
-LABEL org.opencontainers.image.title=$LABEL_TITLE
-ARG LABEL_REVISION
-LABEL org.opencontainers.image.revision=$LABEL_REVISION
-ARG LABEL_SOURCE
-LABEL org.opencontainers.image.source=$LABEL_SOURCE
-ARG LABEL_VENDOR
-LABEL org.opencontainers.image.vendor=$LABEL_VENDOR
-ARG LABEL_URL
-LABEL org.opencontainers.image.url=$LABEL_URL
-ARG LABEL_VERSION
-LABEL org.opencontainers.image.version=$LABEL_VERSION
--- update.sh
+++ update.sh
@@ -9,9 +9,14 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "amd64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-amd64.Dockerfile  && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm-v7.Dockerfile && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM ${image}@.*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile  && echo "${digest}"
+elif [[ ${1} == "tests" ]]; then
+    echo "Listing packages..."
+    docker run --rm --entrypoint="" "${2}" apk -vv info | sort
+    echo "Show version info..."
+    docker run --rm --entrypoint="" "${2}" /app/unpackerr --version
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/davidnewhall/unpackerr/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    sed -i "s/{UNPACKERR_VERSION=[^}]*}/{UNPACKERR_VERSION=${version}}/g" .github/workflows/build.yml
+    echo "VERSION=${version}" > VERSION
     echo "##[set-output name=version;]${version}"
 fi
