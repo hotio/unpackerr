d3073dd2ed1cdcc934a7f13b05fae3da1ecee706
hotio <26902309+hotio@users.noreply.github.com>
2020-10-05 11:14:41 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -17,7 +17,6 @@ elif [[ ${1} == "tests" ]]; then
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/davidnewhall/unpackerr/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    echo "VERSION=${version}" > VERSION
-    echo '{"version":"'"${version}"'"}' > VERSION.json
+    echo '{"version":"'"${version}"'"}' | jq . > VERSION.json
     echo "##[set-output name=version;]${version}"
 fi
