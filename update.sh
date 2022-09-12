882b79a2747a2e464b1ea8cc392c5b04195f2c4e
mrhotio <26902309+mrhotio@users.noreply.github.com>
2022-09-12 13:17:07 +0200

    skip ci

--- update.sh
+++ update.sh
@@ -19,5 +19,6 @@ else
     old_version=$(jq -r '.version' < VERSION.json)
     changelog=$(jq -r '.changelog' < VERSION.json)
     [[ "${old_version}" != "${version}" ]] && changelog="https://github.com/davidnewhall/unpackerr/compare/v${old_version}...v${version}"
-    echo '{"version":"'"${version}"'","changelog":"'"${changelog}"'"}' | jq . > VERSION.json
+    version_json=$(cat ./VERSION.json)
+    jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
 fi
