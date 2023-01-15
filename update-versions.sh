c0b2558f8ac77ce755eca717c0732eaee8e16bbe
mrhotio <26902309+mrhotio@users.noreply.github.com>
2023-01-15 12:13:47 +0100

    change org

--- update-versions.sh
+++ update-versions.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/davidnewhall/unpackerr/commits/master" | jq -r .sha)
+version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/unpackerr/unpackerr/commits/main" | jq -r .sha)
 [[ -z ${version} ]] && exit 0
 old_version=$(jq -r '.version' < VERSION.json)
 changelog=$(jq -r '.changelog' < VERSION.json)
-[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/davidnewhall/unpackerr/compare/${old_version}...${version}"
+[[ "${old_version}" != "${version}" ]] && changelog="https://github.com/unpackerr/unpackerr/compare/${old_version}...${version}"
 version_json=$(cat ./VERSION.json)
 jq '.version = "'"${version}"'" | .changelog = "'"${changelog}"'"' <<< "${version_json}" > VERSION.json
